278c63a2f1d8ec85aab7fbe7e5d7a41414ccc06c
Annie Ochoa <juli.ochoa.199309@gmail.com>
2023-03-11 16:10:10 -0500

    solucion a los ejercicios de js

---
+++ js-1/hello.js
@@ -0,0 +1,17 @@
+function hola(nombre){
+return "Hola " + nombre + "!"
+}
+
+/*function hola(nombre){
+    const frase = "hola " + nombre + "!" 
+    console.log(nombre);
+    return frase
+}*/
+
+// código de prueba
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("")) // "Hola !"
+
+
+
