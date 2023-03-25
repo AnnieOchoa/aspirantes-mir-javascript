4f54d04da4027181455802038deb8cbbc36c1e61
Annie Ochoa <juli.ochoa.199309@gmail.com>
2023-03-25 18:33:17 -0500

    solucion a los ejercicios de js en el navegador-modificado

--- js-brow-1/app.js
+++ js-brow-1/app.js
@@ -1,13 +1,21 @@
 const input = document.querySelector('#texto');
-    const resultado = document.querySelector('#resultado');
+const resultado = document.querySelector('#resultado');
+const resultadoMayuscula = document.querySelector('#resultadoMayusculas');
+let mayusculas = 'Convertir mensaje a mayúsculas'
+let minusculas = 'Convertir mensaje a minúsculas'
 
     input.addEventListener('input' , function () {
-        const texto = input.value.toUpperCase();
+        const texto = input.value;
         resultado.textContent = texto;
+        resultadoMayuscula.textContent = texto.toUpperCase();
     });
 
 const btn = document.querySelector('.btn');    
 
-    btn.addEventListener('click', function(){
-        alert('Se hizo click en el boton');
+    btn.addEventListener('click', function(e){
+        resultadoMayuscula.classList.toggle('hide')
+        resultado.classList.toggle('hide')
+        e.target.textContent === minusculas ? 
+        e.target.textContent = mayusculas :
+        e.target.textContent = minusculas
     });
